f720d37de29387a646217f5b161526a7de405fbf
henriquemanequini <henriquem5@al.insper.edu.br>
2022-05-21 14:18:54 -0300

    ajustando tela inicial

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pygame
 
 pygame.init()
 
-WIDTH = 500
-HEIGHT = 400
+WIDTH = 960
+HEIGHT = 720
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game = True
 
 # ----- Inicia assets
 image = pygame.image.load('tela_inicio_pygame.jpeg').convert()
-image = pygame.transform.scale(image, (800, 800)) #acertar a escala certa da imagem de mudar a width e height se necessario
+image = pygame.transform.scale(image, (960, 720)) #acertar a escala certa da imagem de mudar a width e height se necessario
 
 # ===== Loop principal =====
 while game:
@@ -22,7 +22,7 @@ while game:
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(image, (10, 10))
+    window.blit(image, (0, 0))
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
