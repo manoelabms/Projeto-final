6051806d68a8714249392cf21f1d4aa16b3eeba9
manoelabms <manoelabusatto04@gmail.com>
2022-05-29 14:16:21 -0300

    consertando time rival

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ while status != QUIT:
     if status == INICIAL:
         status = tela_inicial(window)
     if status == TIMES:
-        status, time = tela_times(window)
+        status, time, time_rival = tela_times(window)
     if status == GAME:
-        status = tela_game(window, time)
+        status = tela_game(window, time, time_rival)
     
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
