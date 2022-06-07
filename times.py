036ea08ad185aeaf9f6673d3ec07f946535afac3
henriquemanequini <henriquem5@al.insper.edu.br>
2022-06-07 18:18:04 -0300

    mudança tema

--- times.py
+++ times.py
@@ -21,13 +21,13 @@ def tela_times(window):
             if event.type == pygame.MOUSEBUTTONDOWN:        
                     pos_x, pos_y = pygame.mouse.get_pos()
                     if pos_x > 540 and pos_x < 878 and pos_y > 457 and pos_y < 557:
-                        time_casa = 'Alemanha'
+                        time_casa = 'Real Madrid'
                     elif pos_x > 80 and pos_x < 418 and pos_y > 457 and pos_y < 557:
-                        time_casa = 'Argentina'
+                        time_casa = 'Manchester United'
                     elif pos_x > 80 and pos_x < 353 and pos_y > 230 and pos_y < 330:
-                        time_casa = 'Brasil'
+                        time_casa = 'Bayern Munchen'
                     elif pos_x > 540 and pos_x < 815 and pos_y > 235 and pos_y < 340:
-                        time_casa = 'França'
+                        time_casa = 'Paris Saint-Germain'
 
                     time_rival = random.choice(lista_times)
                     while time_rival == time_casa:
@@ -38,14 +38,14 @@ def tela_times(window):
         window.blit(assets[TIME_BACKGROUND], (0, 0))
     
         cor = (255, 0, 0)
-        vertices_alemanha = (540, 457, 338, 100)
-        pygame.draw.rect(window, cor, vertices_alemanha)
-        vertices_argentina = (80, 457, 338, 100)
-        pygame.draw.rect(window, cor, vertices_argentina)
-        vertices_brasil = (80, 230, 273, 100)
-        pygame.draw.rect(window, cor, vertices_brasil)
-        vertices_franca = (540, 235, 275, 105)
-        pygame.draw.rect(window, cor, vertices_franca)
+        vertices_madrid = (540, 410, 250, 260)
+        pygame.draw.rect(window, cor, vertices_madrid)
+        vertices_united = (190, 410, 250, 260)
+        pygame.draw.rect(window, cor, vertices_united)
+        vertices_bayern = (190, 160, 220, 230)
+        pygame.draw.rect(window, cor, vertices_bayern)
+        vertices_psg = (540, 120, 220, 250)
+        pygame.draw.rect(window, cor, vertices_psg)
 
         # ----- Atualiza estado do jogo
         pygame.display.update()
